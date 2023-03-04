e9dde53b3ed3ed29681855a6b2c1984b9f95c227
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-04 17:30:04 +1300

    /id Added User type

--- src/app/types/user_types.d.ts
+++ src/app/types/user_types.d.ts
@@ -8,3 +8,19 @@ type AuthenticateRequest = {
      */
     password: string
 }
+
+type User = {
+    /**
+     * User email as defined when created
+     */
+    email: string,
+    /**
+     * User first name as defined when created
+     */
+    first_name: string,
+    /**
+     * User last name as defined when created
+     */
+    last_name: string
+
+}
