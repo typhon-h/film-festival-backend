550c9a8e74beeb2f303896b5a6946e04b6f7404e
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-05 00:03:54 +1300

    /id/image Added controller method to fetch image attached to user

--- src/app/controllers/user.image.server.controller.ts
+++ src/app/controllers/user.image.server.controller.ts
@@ -1,12 +1,30 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Logger from "../../config/logger";
+import * as userImages from '../models/user.image.server.model';
+
+import path = require('path');
+import fs = require('fs');
 
 
 const getImage = async (req: Request, res: Response): Promise<void> => {
-    try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+    Logger.http(`GET Retrieve profile image for user: ${req.params.id}`);
+
+    const id = parseInt(req.params.id, 10);
+
+    if (isNaN(id)) {
+        res.status(404).send(`No user with id ${id} found`);
+    }
+
+    try {
+        const [img] = await userImages.getImage(id);
+        const filepath = path.resolve('storage/images/' + img.image_filename);
+
+        if (fs.existsSync(filepath)) {
+            res.status(200).sendFile(filepath);
+        } else {
+            res.status(404).send(`Image could not be found`);
+        }
+
         return;
     } catch (err) {
         Logger.error(err);
@@ -18,7 +36,7 @@ const getImage = async (req: Request, res: Response): Promise<void> => {
 
 
 const setImage = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -33,7 +51,7 @@ const setImage = async (req: Request, res: Response): Promise<void> => {
 
 
 const deleteImage = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -46,4 +64,4 @@ const deleteImage = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
-export {getImage, setImage, deleteImage}
+export { getImage, setImage, deleteImage }
