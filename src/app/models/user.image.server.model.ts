9e6abebfeedc900891aab6ce975fa2fdbacfaea7
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-05 01:28:02 +1300

    /id/image Added model method for updating user images

--- src/app/models/user.image.server.model.ts
+++ src/app/models/user.image.server.model.ts
@@ -3,7 +3,7 @@ import Logger from '../../config/logger';
 import { ResultSetHeader } from 'mysql2';
 
 
-const getImage = async (id: number): Promise<Image[]> => {
+const getOne = async (id: number): Promise<Image[]> => {
     Logger.info(`Retrieving filepath for profile picture of user ${id}`);
     const conn = await getPool().getConnection();
     const query = "select image_filename from user where id = ?";
@@ -12,6 +12,13 @@ const getImage = async (id: number): Promise<Image[]> => {
     return result;
 };
 
+const alter = async (id: number, filename: string): Promise<ResultSetHeader> => {
+    Logger.info(`Updating profile image for user ${id}`);
+    const conn = await getPool().getConnection();
+    const query = "update user set image_filename = ? where id = ?";
+    const [result] = await conn.query(query, [filename, id]);
+    await conn.release();
+    return result;
+}
 
-
-export { getImage }
+export { getOne, alter }
