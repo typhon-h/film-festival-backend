5356d6c039ff176f68836a02c1fd8f516014a6fa
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-05 15:31:11 +1300

    /id/image Added partial controller method for updating image

--- src/app/controllers/user.image.server.controller.ts
+++ src/app/controllers/user.image.server.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import Logger from "../../config/logger";
 import * as userImages from '../models/user.image.server.model';
-
+import { isAuthenticated } from '../controllers/user.server.controller';
 import path = require('path');
 import fs = require('fs');
+const imagesPath = 'storage/images/';
+const allowedFiletypes = ['png', 'gif', 'jpeg'];
 
 
 const getImage = async (req: Request, res: Response): Promise<void> => {
@@ -16,8 +18,8 @@ const getImage = async (req: Request, res: Response): Promise<void> => {
     }
 
     try {
-        const [img] = await userImages.getImage(id);
-        const filepath = path.resolve('storage/images/' + img.image_filename);
+        const [img] = await userImages.getOne(id);
+        const filepath = path.resolve(imagesPath + img.image_filename);
 
         if (fs.existsSync(filepath)) {
             res.status(200).sendFile(filepath);
@@ -36,10 +38,42 @@ const getImage = async (req: Request, res: Response): Promise<void> => {
 
 
 const setImage = async (req: Request, res: Response): Promise<void> => {
+    const token = req.headers['x-authorization'];
+    if (token === undefined) {
+        res.status(401).send("Unauthorized.");
+        return;
+    }
+
+    const id = parseInt(req.params.id, 10);
+    if (isNaN(id)) {
+        res.status(404).send(`No user with id ${id}`);
+        return;
+    }
+
+    // Removing "image/"
+    const filetype = req.headers['content-type'].replace('image/', '');
+    if (!req.headers['content-type'].includes('image/')
+        || !allowedFiletypes.includes(filetype)) {
+        res.status(400).send('Bad Request. Invalid image supplied');
+        return;
+    }
+
     try {
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+        if (!((await isAuthenticated(id, token.toString())).valueOf())) {
+            res.status(403).send("Forbidden. Cannot change another user's profile picture");
+            return;
+        }
+
+        const filename = 'user_' + id;
+        const result = await userImages.alter(id, filename + '.' + filetype);
+
+        if (result.affectedRows === 0) { // TODO: This will always be caught by 403
+            res.status(404).send("User not found.");
+            return;
+        }
+
+        // Created vs Updated
+
         return;
     } catch (err) {
         Logger.error(err);
