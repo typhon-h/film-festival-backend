330f6f6c9f8a87206d37e9e0d75a5449250d8359
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-16 23:15:00 +1300

    /films/id Added getOne model method and minor modifications to /films get all

--- src/app/models/film.server.model.ts
+++ src/app/models/film.server.model.ts
@@ -54,9 +54,9 @@ const getAll = async (
         params.push(directorId);
     }
 
-    switch (sortBy) { // TODO: GET /films (sortBy RATING_DESC) works if secondary sort by id
+    switch (sortBy) {
         case "ALPHABETICAL_ASC":
-            query += "title ASC ";
+            query += " order by title ASC ";
             break;
         case "ALPHABETICAL_DESC":
             query += " order by title DESC ";
@@ -77,11 +77,26 @@ const getAll = async (
             query += " order by release_date ASC ";
             break;
     }
+    query += " , filmId ASC"; //id secondary sort
 
     const [result] = await conn.query(query, params);
     await conn.release();
     return result;
 
+}
+
+const getOne = async (id: number): Promise<Film[]> => {
+    const conn = await getPool().getConnection();
+    const query = "select film.id as filmId, title, description, runtime, genre_id as genreId, "
+        + " age_rating as ageRating, release_date as releaseDate, user.id as directorId, user.first_name as directorFirstName,  "
+        + " user.last_name as directorLastName, cast(coalesce(rating,0) as float) as rating, coalesce(numReviews,0) as numReviews  "
+        + " from film inner join user on film.director_id=user.id "
+        + " left outer join (select film_id, round(avg(rating),2) as rating from film_review group by film_id) as ratings on ratings.film_id=film.id "
+        + " left outer join (select film_id, count(*) as numReviews from film_review group by film_id) as reviews on reviews.film_id=film.id "
+        + " where film.id = ?";
+    const [result] = await conn.query(query, [id]);
+    await conn.release();
+    return result;
 }
 
 const getAllGenres = async (): Promise<Genre[]> => {
@@ -93,4 +108,4 @@ const getAllGenres = async (): Promise<Genre[]> => {
 }
 
 
-export { getAll, getAllGenres }
+export { getAll, getOne, getAllGenres }
