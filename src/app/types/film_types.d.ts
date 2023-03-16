74beb0735f111e568072de157b0b7a820b341732
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-16 14:49:05 +1300

    /films Added FilmResult type for getAll

--- src/app/types/film_types.d.ts
+++ src/app/types/film_types.d.ts
@@ -32,3 +32,42 @@ type Film = {
      */
     release_date: string
 }
+
+type FilmResult = {
+    /**
+     * Id of the film
+     */
+    filmId: number,
+    /**
+     * Title of the film as defined when created
+     */
+    title: string,
+    /**
+     * Id of the genre of the film
+     */
+    genreId: number,
+    /**
+     * Age classification restricted to an enum
+     */
+    ageRating: string,
+    /**
+     * Id of the director of the film
+     */
+    directorId: number,
+    /**
+     * First name of the director of the film
+     */
+    directorFirstName: string,
+    /**
+     * 
+     */
+    directorLastName: string,
+    /**
+     * Average rating across reviews
+     */
+    rating: number,
+    /**
+     * Date film was released
+     */
+    releaseDate: string
+}
