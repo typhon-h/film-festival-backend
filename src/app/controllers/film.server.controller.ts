57e995f3f3625a3d9904a925a91a37be5efd89e8
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-16 14:49:34 +1300

    /films Added controller method to get all films based on criteria

--- src/app/controllers/film.server.controller.ts
+++ src/app/controllers/film.server.controller.ts
@@ -1,12 +1,63 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Logger from "../../config/logger";
+import * as validator from './validate.server';
+import * as films from '../models/film.server.model';
+
 
 
 const viewAll = async (req: Request, res: Response): Promise<void> => {
-    try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+    Logger.http(`GET all films that match body criteria`);
+    const validation = await validator.validate(
+        validator.schemas.film_search,
+        req.query
+    );
+
+    if (validation !== true) {
+        res.statusMessage = `Bad Request: ${validation.toString()}`;
+        res.status(400).send();
+        return;
+    }
+
+    try {
+        const search = (req.query.q !== undefined ? req.query.q.toString() : undefined);
+        const directorId = (req.query.directorId !== undefined ? parseInt(req.query.directorId.toString(), 10) : undefined);
+        const reviewerId = (req.query.reviewerId !== undefined ? parseInt(req.query.reviewerId.toString(), 10) : undefined);
+        const sort = (req.query.sortBy !== undefined ? req.query.sortBy as string : undefined);
+        let genreIds;
+        let ageRatings;
+
+        if (Array.isArray(req.query.genreIds)) {
+            genreIds = req.query.genreIds.map((str): number => {
+                return parseInt(str.toString(), 10);
+            });
+        } else if (req.query.genreIds !== undefined) {
+            genreIds = [parseInt(req.query.genreIds as string, 10)];
+        }
+
+        if (Array.isArray(req.query.ageRatings)) {
+            ageRatings = req.query.ageRatings.map((str): string => {
+                return str.toString();
+            });
+        } else if (req.query.ageRatings !== undefined) {
+            ageRatings = [req.query.ageRatings.toString()];
+        }
+
+        const result = await films.getAll(search, genreIds, ageRatings, directorId, reviewerId, sort)
+
+        const startIndex = (req.query.startIndex !== undefined ? parseInt(req.query.startIndex.toString(), 10) : undefined);
+        const count = (req.query.count !== undefined ? parseInt(req.query.count.toString(), 10) : result.length);
+
+        let end;
+        if (startIndex === undefined && count !== 0) {
+            end = count;
+        } else if (count === 0) {
+            end = result.length;
+        } else {
+            end = startIndex + count; //TODO: FIX
+        }
+
+
+        res.status(200).send({ "films": result.slice(startIndex, end), "count": result.length });
         return;
     } catch (err) {
         Logger.error(err);
@@ -17,7 +68,7 @@ const viewAll = async (req: Request, res: Response): Promise<void> => {
 }
 
 const getOne = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -31,7 +82,7 @@ const getOne = async (req: Request, res: Response): Promise<void> => {
 }
 
 const addOne = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -45,7 +96,7 @@ const addOne = async (req: Request, res: Response): Promise<void> => {
 }
 
 const editOne = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -59,7 +110,7 @@ const editOne = async (req: Request, res: Response): Promise<void> => {
 }
 
 const deleteOne = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -73,7 +124,7 @@ const deleteOne = async (req: Request, res: Response): Promise<void> => {
 }
 
 const getGenres = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -86,4 +137,4 @@ const getGenres = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
-export {viewAll, getOne, addOne, editOne, deleteOne, getGenres};
+export { viewAll, getOne, addOne, editOne, deleteOne, getGenres };
