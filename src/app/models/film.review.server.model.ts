b36b3cd3ee649c1145c496e2d64a9ddd3d98b231
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-18 01:38:39 +1300

    /films/id/review Added order by to getAll method

--- src/app/models/film.review.server.model.ts
+++ src/app/models/film.review.server.model.ts
@@ -6,8 +6,10 @@ const getAll = async (id: number): Promise<Review[]> => {
     Logger.info(`Getting all films that match criteria`);
 
     const conn = await getPool().getConnection();
-    const query = "select film_id, user.id as reviewerId, user.first_name as reviewerFirstName, user.last_name as reviewerLastName,rating, review, timestamp "
-        + " from film_review join user on user.id = film_review.user_id where film_id = ?";
+    const query = "select film_id, user.id as reviewerId, user.first_name as reviewerFirstName, "
+        + " user.last_name as reviewerLastName,rating, review, timestamp "
+        + " from film_review join user on user.id = film_review.user_id where film_id = ? "
+        + " order by timestamp desc";
 
     const [result] = await conn.query(query, [id]);
     await conn.release();
