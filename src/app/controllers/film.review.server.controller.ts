f614341144a648c0af584de8fff2661d6a004d97
Harrison Tyson <hty13@uclive.ac.nz>
2023-03-18 01:52:37 +1300

    /films/id/review Add getReviews controller method

--- src/app/controllers/film.review.server.controller.ts
+++ src/app/controllers/film.review.server.controller.ts
@@ -1,13 +1,35 @@
-import {Request, Response} from "express";
+import { Request, Response } from "express";
 import Logger from "../../config/logger";
+import * as reviews from "../models/film.review.server.model";
+import { getOne } from "../models/film.server.model";
 
 
 const getReviews = async (req: Request, res: Response): Promise<void> => {
-    try{
-        // Your code goes here
-        res.statusMessage = "Not Implemented Yet!";
-        res.status(501).send();
+    Logger.http(`GET all reviews for film ${req.params.id}`);
+
+    const id = parseInt(req.params.id, 10);
+
+    if (isNaN(id)) {
+        res.statusMessage = `Bad Request: invalid id ${req.params.id}`;
+        res.status(400).send();
         return;
+    }
+
+    try {
+
+        const film = await getOne(id);
+        if (film === undefined) {
+            res.status(404).send(`Not Found. No film found with id ${id}`);
+            return;
+        }
+
+        const result = await reviews.getAll(id);
+        if (result !== undefined) {
+            res.status(200).send(result);
+            return;
+        } else {
+            throw new Error(`Reviews could not be retrieved`);
+        }
     } catch (err) {
         Logger.error(err);
         res.statusMessage = "Internal Server Error";
@@ -18,7 +40,7 @@ const getReviews = async (req: Request, res: Response): Promise<void> => {
 
 
 const addReview = async (req: Request, res: Response): Promise<void> => {
-    try{
+    try {
         // Your code goes here
         res.statusMessage = "Not Implemented Yet!";
         res.status(501).send();
@@ -33,4 +55,4 @@ const addReview = async (req: Request, res: Response): Promise<void> => {
 
 
 
-export {getReviews, addReview}
+export { getReviews, addReview }
